b1c3c39dfd9d977659b3266546db230b43af8827
Pushkar <indianfouji888@gmail.com>
2025-04-15 17:58:35 +0530

    Updates

---
+++ precache-manifest.b57cd7a558217ec40fa4fa08b0b3b3bb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6151e98bb6a0af0a446d4127a891b9",
+    "url": "/birthday-surprise/index.html"
+  },
+  {
+    "revision": "2ed5dad7f8dc0b91f29b",
+    "url": "/birthday-surprise/static/css/main.a03a5936.chunk.css"
+  },
+  {
+    "revision": "864f142a5e348a38b687",
+    "url": "/birthday-surprise/static/js/2.8461cebe.chunk.js"
+  },
+  {
+    "revision": "0f094fe6035d12dd83739e56fae72513",
+    "url": "/birthday-surprise/static/js/2.8461cebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e7001023aeb998e2e1",
+    "url": "/birthday-surprise/static/js/3.16f0790f.chunk.js"
+  },
+  {
+    "revision": "2ed5dad7f8dc0b91f29b",
+    "url": "/birthday-surprise/static/js/main.aecbff39.chunk.js"
+  },
+  {
+    "revision": "dfda793e69b96ea8130b",
+    "url": "/birthday-surprise/static/js/runtime-main.f53bd995.js"
+  },
+  {
+    "revision": "b70d0cfa514c6d9a889650625ce31c66",
+    "url": "/birthday-surprise/static/media/gift-image.b70d0cfa.jpg"
+  },
+  {
+    "revision": "21ce305b733eceae5d05e23d53552c26",
+    "url": "/birthday-surprise/static/media/gift-music.21ce305b.mp3"
+  },
+  {
+    "revision": "326bc232bfafea1aba2465c369f04827",
+    "url": "/birthday-surprise/static/media/loveyou.326bc232.mp3"
+  },
+  {
+    "revision": "cc90e0c01a1f9fbba076c42f1adf4261",
+    "url": "/birthday-surprise/static/media/music.cc90e0c0.mp3"
+  },
+  {
+    "revision": "e1b6651f597ff09b3ef96a78835e7b4b",
+    "url": "/birthday-surprise/static/media/perfect.e1b6651f.mp3"
+  },
+  {
+    "revision": "c479f8d839ba6f9d9e8ff1fc4856f489",
+    "url": "/birthday-surprise/static/media/vasu-love.c479f8d8.jpg"
+  }
+]);
