40e95be61a4ff33609f8f882d3cda7a004f237f4
Pushkar <indianfouji888@gmail.com>
2025-04-15 21:03:01 +0530

    Updates

---
+++ precache-manifest.1a31a665d54a9b352ecac9eb57b147d7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc4a3782f195b3878eeaa7ffaeb7d9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cad9d55f4a2a3379b20",
+    "url": "/static/css/main.a03a5936.chunk.css"
+  },
+  {
+    "revision": "b52da36dd762b5e7763d",
+    "url": "/static/js/2.e9e69573.chunk.js"
+  },
+  {
+    "revision": "0f094fe6035d12dd83739e56fae72513",
+    "url": "/static/js/2.e9e69573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70e7001023aeb998e2e1",
+    "url": "/static/js/3.16f0790f.chunk.js"
+  },
+  {
+    "revision": "2cad9d55f4a2a3379b20",
+    "url": "/static/js/main.750f0955.chunk.js"
+  },
+  {
+    "revision": "4546896fec6b1739f090",
+    "url": "/static/js/runtime-main.afee8626.js"
+  },
+  {
+    "revision": "b70d0cfa514c6d9a889650625ce31c66",
+    "url": "/static/media/gift-image.b70d0cfa.jpg"
+  },
+  {
+    "revision": "21ce305b733eceae5d05e23d53552c26",
+    "url": "/static/media/gift-music.21ce305b.mp3"
+  },
+  {
+    "revision": "326bc232bfafea1aba2465c369f04827",
+    "url": "/static/media/loveyou.326bc232.mp3"
+  },
+  {
+    "revision": "cc90e0c01a1f9fbba076c42f1adf4261",
+    "url": "/static/media/music.cc90e0c0.mp3"
+  },
+  {
+    "revision": "e1b6651f597ff09b3ef96a78835e7b4b",
+    "url": "/static/media/perfect.e1b6651f.mp3"
+  },
+  {
+    "revision": "c479f8d839ba6f9d9e8ff1fc4856f489",
+    "url": "/static/media/vasu-love.c479f8d8.jpg"
+  }
+]);
